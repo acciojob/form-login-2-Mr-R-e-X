18557c2d9ec057159a6521caa4f96117546fdfe6
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-27 00:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function alertDtls(event) {
+  event.preventDefault();
+  let ipDtls = document.querySelectorAll(".ip-dtls");
+  let contactDtls = [];
+  ipDtls.forEach((input) => {
+    contactDtls.push(input.value);
+  });
+  window.alert(
+    `First Name: ${contactDtls[0]} Last Name: ${contactDtls[1]} Phone Number: ${contactDtls[2]} Email Id: ${contactDtls[3]}`
+  );
+}
+
+document.getElementById("contact-form").onsubmit = alertDtls;
